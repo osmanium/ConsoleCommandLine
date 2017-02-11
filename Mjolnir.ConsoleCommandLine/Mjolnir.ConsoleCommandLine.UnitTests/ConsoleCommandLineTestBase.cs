57ca3c75a8ccb2b85281effdff4cfd7954038c6a
Osman GUMUS <osmangumus@outlook.com>
2017-02-11 18:26:55 +0400

    test method is created

---
+++ Mjolnir.ConsoleCommandLine/Mjolnir.ConsoleCommandLine.UnitTests/ConsoleCommandLineTestBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mjolnir.ConsoleCommandLine.UnitTests
+{
+    [TestClass]
+    public class ConsoleCommandLineTestBase
+    {
+        [TestInitialize]
+        public void InitalizeCMD()
+        {
+            var cmd = ConsoleCommandLine.Instance;
+
+            cmd.Initialize();
+        }
+    }
+}
--- Mjolnir.ConsoleCommandLine/Mjolnir.ConsoleCommandLine.UnitTests/ConsoleCommandLineTests.cs
+++ Mjolnir.ConsoleCommandLine/Mjolnir.ConsoleCommandLine.UnitTests/ConsoleCommandLineTests.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mjolnir.ConsoleCommandLine.UnitTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Mjolnir.ConsoleCommandLine.UnitTests
 {
     [TestClass]
-    public class ConsoleCommandLineTests
+    public class ConsoleCommandLineTests : ConsoleCommandLineTestBase
     {
         [TestMethod]
-        public void InitalizeCMD()
+        public void ClearTest()
         {
             var cmd = ConsoleCommandLine.Instance;
-
-            cmd.Initialize();
-
-            cmd.Run(null);
+            cmd.ProcessLine("clear");
         }
     }
 }
